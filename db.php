7462ce2e9840196135d1b01d0d00848e20fb7ef6
jccummi1 <jccummi1@mtu.edu>
2022-10-07 13:28:25 -0400

    Create db.php
    created db.php and added its first function

---
+++ db.php
@@ -0,0 +1,10 @@
+<html>
+<?php
+function connectDB() {
+    $config = parse_ini_file("db.ini");
+    $dbh = new PDO($config['dsn'], $config['username'], $config['password']);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    return $dbh;
+}
+?>
+</html> 
