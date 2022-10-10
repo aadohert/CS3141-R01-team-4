1c111361469eb03c988980b12a8738e983322adb
btfuhrma <112784727+btfuhrma@users.noreply.github.com>
2022-10-10 13:01:09 -0400

    Add files via upload
    Bare bones file for the website design

---
+++ Index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="eng">
+    <head>
+        <style>
+            a:visited{
+                text-decoration: none;
+                color: black;
+            }
+            a{
+                text-decoration: none;
+            }
+            th#Icon{
+                text-align: left;
+            }
+            th.navbar-right-align{
+                text-align: right;
+            }
+            .form-group{
+                margin-bottom: 10px;
+            }
+            #starNameDiv{
+                margin-bottom: 75px;
+            }
+            .divOffSet{
+                margin-left: 15px;
+            }
+        </style>
+        <title>Star Finder</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1"> 
+    </head>
+
+    <body>
+            <!-- This div is used for setting up the table for the navigation bar.  -->
+            <div>
+                <table class="navbar-table" width="100%">
+                    <col style="width: 70%">
+                    <col style="width: 20%">
+                    <col style="width: 10%">
+                    <tr>
+                        <th id="Icon"><h1><a href="/Index.php" style="margin-left: 15px;">Star Finder</a></h1></th>
+                        <th class="navbar-right-align"><h3><a href="">Favorites</a></h3></th>
+                        <th class="navbar-right-align"><button onclick="login()" style="background-image: url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjrCdY7vbLNb3uuqCixRviazh7zdc0yUSB3Ou2w27iCQRKN6T1ylCGuCs1YXkTOQBTjzM&usqp=CAU'); color:white; cursor:pointer; width:75px;height:35px;">Login</button></th>
+                    </tr>
+                </table>
+            </div>
+            <hr>
+            <!--Div for inputting the star name-->
+            <div id="starNameDiv" class="divOffSet">
+                <form style="margin-top:20px;">
+                    <div class="form-group">
+                        <input class="form-control" type="text" id="starName" name="starName" placeholder="Star Name" style="width: 150px">
+                    </div>
+                    <div class="form-group">
+                        <input class="form-control" type="submit" value="Calculate"> 
+                    </div>  
+                </form>
+
+            </div>
+            <!--Div for inputting the RA and Dec-->
+            <div class="divOffSet">
+               <form>
+                    <div class="form-group">
+                        <input class="form-control" type="text" id="starName" name="starName" placeholder="RA" style="width: 150px">
+                    </div>
+                    <div class="form-group">
+                        <input class="form-control" type="text" id="starName" name="starName" placeholder="Dec" style="width: 150px">
+                    </div>
+                    <div class="form-group">
+                        <input class="form-control" type="submit" value="Calculate"> 
+                    </div>  
+                </form>
+            </div>
+    </body>
+</html>
