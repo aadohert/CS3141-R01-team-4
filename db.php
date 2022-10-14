0268396e30717fa8f3fa6b13fd6bf543fc52616a
btfuhrma <112784727+btfuhrma@users.noreply.github.com>
2022-10-13 20:00:32 -0400

    Update db.php
    Fixed alignment with navbar potentially

--- db.php
+++ db.php
@@ -23,7 +23,7 @@ function authenticate($user, $passwd) {
 function printTopBanner() {
     echo 
     '<div> 
-        <table class="navbar-table" width="100%" style="margin-top:15px;">
+        <table class="navbar-table" width="100%">
             <col style = "width: 70%">
             <col style="width: 20%">
             <col style="width: 10%">
@@ -35,7 +35,7 @@ function printTopBanner() {
         
              
     if(isset($_SESSION["user"])) echo '<th class="navbar-right-align"><button onclick="location.href = \'logout.php\';" style="background-image: url(\'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjrCdY7vbLNb3uuqCixRviazh7zdc0yUSB3Ou2w27iCQRKN6T1ylCGuCs1YXkTOQBTjzM&usqp=CAU\'); color:white; cursor:pointer; width:75px;height:35px;">Log Out</button></th>';
-    else echo '<th class="navbar-right-align"><button onclick="location.href = \'login.php\';" style="background-image: url(\'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjrCdY7vbLNb3uuqCixRviazh7zdc0yUSB3Ou2w27iCQRKN6T1ylCGuCs1YXkTOQBTjzM&usqp=CAU\'); color:white; cursor:pointer; width:75px;height:35px;">Login</button></th>';
+    else echo '<th class="navbar-right-align" style="vertical-align: top;"><button onclick="location.href = \'login.php\';" style="background-image: url(\'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjrCdY7vbLNb3uuqCixRviazh7zdc0yUSB3Ou2w27iCQRKN6T1ylCGuCs1YXkTOQBTjzM&usqp=CAU\'); color:white; cursor:pointer; width:75px;height:35px;">Login</button></th>';
         
     echo 
             '</tr>
