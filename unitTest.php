612545c32ad6faa30aae2315d3db1929367f2eef
NickyFranklin <nafrankl@mtu.edu>
2022-10-17 13:58:57 -0400

    Finished testing of dayssince funciton

--- unitTest.php
+++ unitTest.php
@@ -28,11 +28,59 @@
                  $realValue = daysSince2000($testDate);
                  return assert($expectedValue == $realValue);
              }
+
+             function testjan2200True() {
+                $expectedValue = '1';
+                 $testDate = strtotime('02-01-2000');
+                 $realValue = daysSince2000($testDate);
+                 return assert($expectedValue == $realValue);
+             }
+
+             function testjan22000False() {
+                $expectedValue = '-730';
+                 $testDate = strtotime('02-01-2000');
+                 $realValue = daysSince2000($testDate);
+                 return assert($expectedValue != $realValue);
+             }
+
+             function testoctober282001True() {
+                $expectedValue = '666';
+                 $testDate = strtotime('28-10-2001');
+                 $realValue = daysSince2000($testDate);
+                 return assert($expectedValue == $realValue);
+             }
+
+             function testoct282001False() {
+                $expectedValue = '-730';
+                $testDate = strtotime('28-10-2001');
+                $realValue = daysSince2000($testDate);
+                 return assert($expectedValue != $realValue);
+             }
+
+             function testapr42022True() {
+                $expectedValue = '8129';
+                 $testDate = strtotime('04-04-2022');
+                 $realValue = daysSince2000($testDate);
+                 return assert($expectedValue == $realValue);
+             }
+
+             function testapr42022False() {
+                $expectedValue = '-730';
+                 $testDate = strtotime('04-04-2022');
+                 $realValue = daysSince2000($testDate);
+                 return assert($expectedValue != $realValue);
+             }
         ?>
         <?php
             echo "Test 1:". testjan12000();
             echo "<br>". "Test 2:". testjan11998False();
             echo "<br>". "Test 3:". testjan11998True();
+            echo "<br>". "Test 4:". testjan2200True();
+            echo "<br>". "Test 5:". testjan22000False();
+            echo "<br>". "Test 6:". testoctober282001True();
+            echo "<br>". "Test 7:". testoct282001False();
+            echo "<br>". "Test 8:". testapr42022True();
+            echo "<br>". "Test 9:". testapr42022False();
         ?>
     </body>
 </html>
