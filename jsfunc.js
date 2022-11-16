f6cb33cf6bc06a8dcb127b0dc204bad9c5561839
btfuhrma <112784727+btfuhrma@users.noreply.github.com>
2022-11-16 13:34:37 -0500

    added comments to js functions
    added comments to the js functions

--- jsfunc.js
+++ jsfunc.js
@@ -1,6 +1,12 @@
-/**@abstract
- * This function is called when the checkbox is checked or unchecked and changes to darkmode depending on its status
- * 
+/**
+ * Checks the slider to tell what theme to make the website.
+ *
+ * This function is called when the slider is checked or unchecked which it then changes the theme of the website. It does this by going through the elements and inverting the color of specific elements.
+ *
+ * @access     public
+ *
+ * @global
+ *
  */
  function darkmode(){
     var element = document.body;
@@ -35,16 +41,51 @@
         localStorage.removeItem("dark-mode");
     }
 }
+
+/**
+ * Checks what theme that the page was most recently in.
+ *
+ * This function checks what the theme of the website was last in when and calls the darkmode function depending on the state.
+ *
+ * @access     public
+ *
+ * @global
+ *
+ * @see darkmode
+ */
 function themeChecker(){
     if(localStorage.getItem("dark-mode")){
         document.getElementById("slider").checked = true;
         darkmode();
     }
 }
+
+/**
+ * Every time a character is inputted into the starName input and corresponds to star, this function updates the input and removes the current elements in the list.
+ *
+ * This function updates the value of the autofill input and removes the current elements from the autofill list.
+ *
+ * @access     public
+ *@see removeElements
+ * @global
+ *
+ */
 function displayNames(value){
     autoFillInput.value = value;
     removeElements();
 }
+
+
+/**
+ * Removes the elements from the autofill list.
+ *
+ * Goes through the elements of the autofill list items and remvoes them.
+ *
+ * @access     public
+ *
+ * @global
+ *
+ */
 function removeElements(){
     let items = document.querySelectorAll(".list-items");
     items.forEach((item) => {
