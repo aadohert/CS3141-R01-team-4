5aa9c6bf9547c97348f1008b8b6b7d1c4e1f53cc
btfuhrma <112784727+btfuhrma@users.noreply.github.com>
2022-11-30 13:24:27 -0500

    admin update

--- admin.php
+++ admin.php
@@ -19,38 +19,38 @@
                 printTopBanner();
             ?>
             <hr>
-            <table class="admin-page-org">
+            <table class="admin-page-org" style="width: 100%;">
                 <col style="width:50%">
-                <col style="width: 50%;"
+                <col style="width:50%">
                 <tr class="admin-star-input">
                     <td>
                         <form>
                             <div class="admin-container">
                                 <div class="admin-star-input-data">
-                                    <input type="text" placeholder="Star Name">
+                                    <input type="text" placeholder="Star Name" class="admin-star-align" name="adminStarName">
                                 </div>
                                 <div class="admin-star-input-data">
-                                    <input type="text" placeholder="RA">
+                                    <input type="text" placeholder="RA" class="admin-star-align" name="adminRA">
                                 </div>
                                 <div class="admin-star-input-data">
-                                    <input type="text" placeholder="DEC">
+                                    <input type="text" placeholder="DEC" class="admin-star-align" name="adminDEC">
                                 </div>
                                 <div class="admin-star-input-data">
-                                    <input type="text" placeholder="Constellation">
+                                    <input type="text" placeholder="Const" class="admin-star-align" name="adminConst">
                                 </div>
                                 <div class="admin-star-input-data">
-                                    <input type="text" placeholder="Description">
+                                    <textarea placeholder="Description" class="admin-star-align admin-description" name="adminDesc"></textarea>
                                 </div>
                                 <div class="admin-star-input-data">
-                                    <input type="submit" name="submit" placeholder="Submit">
+                                    <input type="submit" placeholder="Submit" class="admin-star-align" name="adminSubmit">
                                 </div>
                             </div>
                         </form>
                     </td>
-                </tr>
-                <tr>
                     <td>
-
+                        <div>
+                            <p>Test</p>
+                        </div>
                     </td>
                 </tr>
             </table>
