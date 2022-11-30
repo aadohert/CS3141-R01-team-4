6e62c6881e9165229271bcc34998823d3d17d158
btfuhrma <112784727+btfuhrma@users.noreply.github.com>
2022-11-30 13:34:38 -0500

    Update admin.php

--- admin.php
+++ admin.php
@@ -3,7 +3,9 @@
 <?php
     session_start();
     require "db.php";
-    
+    if(isAdmin($_SESSION["user"]) == false || !(isset($_SESSION["user"]))){
+        header('LOCATION: Index.php');
+    }
     
 ?>    
     <head>
